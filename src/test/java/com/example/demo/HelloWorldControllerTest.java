d963c69069d9a5b11fe7ccb04724f3d1f5c38d40
avillega <villegas.pelaez.andres@gmail.com>
2018-08-09 16:35:22 -0500

    initial commit

---
+++ src/test/java/com/example/demo/HelloWorldControllerTest.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloWorldControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void testHelloWorld() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello")
+        .accept(MediaType.TEXT_HTML))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World!"));
+    }
+}
